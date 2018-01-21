2a69b98f56ec1aca023f696622f0039fcde8f638
Ifeoluwa Abimbola <david.abimbola@aol.com>
2018-01-21 11:12:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logshout(string) {
-  console.log(logshout(HELLO))
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
 
-function logwhisper(string) {
-  console.log(HELLO)
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  console.log(string)
-}
-
- sayHiToGrandma('hello')
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+  return "I love you, too."
+  }
+  
+  }
